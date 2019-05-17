2255ef837851989bda1f4c63e180ccf35a1cac14
wilson <yxw_android@163.com>
2019-05-17 13:25:36 +0800

    add  Video

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'integratedFlutter' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for integratedFlutter
+
+  target 'integratedFlutterTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'integratedFlutterUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
+#target 'integratedFlutter' do
+#  flutter_application_path = '/Users/xiangwenwen/integratedFlutter/my_flutter/'
+#  eval(File.read(File.join(flutter_application_path, '.ios', 'Flutter', 'podhelper.rb')), binding)
+#end
+
+flutter_application_path = '/Users/wilson/Downloads/integratedFlutter/my_flutter'
+eval(File.read(File.join(flutter_application_path, '.ios', 'Flutter', 'podhelper.rb')), binding)
--- integratedFlutter/ViewController.swift
+++ integratedFlutter/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Flutter
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let button = UIButton(type:UIButton.ButtonType.custom)
+        button.addTarget(self, action: #selector(handleButtonAction), for: .touchUpInside)
+        button.setTitle("Press me", for: UIControl.State.normal)
+        button.frame = CGRect(x: 80.0, y: 210.0, width: 160.0, height: 40.0)
+        button.backgroundColor = UIColor.blue
+        self.view.addSubview(button)
     }
-
-
+    
+    @objc func handleButtonAction() {
+        let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine;
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)!;
+        self.present(flutterViewController, animated: false, completion: nil)
+    }
 }
-
